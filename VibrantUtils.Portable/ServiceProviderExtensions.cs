61ec35a34965f513c41cb8dcb6d077f04bcae623
anurse <andrew@andrewnurse.net>
2012-06-02 00:39:49 -0700

    Added ServiceProviderExtensions

---
+++ VibrantUtils.Portable/ServiceProviderExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System
+{
+    public static class ServiceProviderExtensions
+    {
+        public static T GetService<T>(this IServiceProvider self)
+        {
+            object service = self.GetService(typeof(T));
+            return service == null ? default(T) : (T)service;
+        }
+    }
+}
